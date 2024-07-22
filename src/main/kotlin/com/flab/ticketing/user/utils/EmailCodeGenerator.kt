8a475e707685622df96385e8f6c478efc3d88680
minturtle <minseok0425@kakao.com>
2024-07-22 19:45:17 +0900

    feat: Email 인증 코드 생성 기능 개발

--- src/main/kotlin/com/flab/ticketing/user/utils/EmailCodeGenerator.kt
+++ src/main/kotlin/com/flab/ticketing/user/utils/EmailCodeGenerator.kt
@@ -1,4 +1,16 @@
 package com.flab.ticketing.user.utils
 
+import org.springframework.stereotype.Component
+
+@Component
 class EmailCodeGenerator {
+
+    fun createEmailCode() : String{
+        val charset = "ABCDEFGHIJKLMNOPQRSTUVWXTZ0123456789"
+        return (1..6)
+            .map { charset.random() }
+            .joinToString("")
+
+    }
+
 }
---
+++ src/test/kotlin/com/flab/ticketing/user/utils/EmailCodeGeneratorTest.kt
@@ -0,0 +1,25 @@
+package com.flab.ticketing.user.utils
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.string.shouldMatch
+
+class EmailCodeGeneratorTest : BehaviorSpec(){
+
+    val emailCodeGenerator : EmailCodeGenerator = EmailCodeGenerator()
+
+    init {
+
+        given("이메일 코드 생성 요청 시"){
+
+            `when`("이메일 생성 코드를 호출하면"){
+                val actual = emailCodeGenerator.createEmailCode()
+
+                `then`("랜덤한 6자리의 영문(대문자)과 숫자를 포함한 문자열을 생성할 수 있다."){
+                    actual shouldMatch "^(?=.*[A-Z])(?=.*\\d)[A-Z\\d]{6}\$"
+                }
+            }
+        }
+    }
+
+
+}
