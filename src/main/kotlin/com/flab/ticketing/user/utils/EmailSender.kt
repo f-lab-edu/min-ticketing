346202339c7192d16a800aee75779db3e390439a
minturtle <minseok0425@kakao.com>
2024-07-22 20:01:28 +0900

    feat: 이메일 전송 기능 개발

--- src/main/kotlin/com/flab/ticketing/user/utils/EmailSender.kt
+++ src/main/kotlin/com/flab/ticketing/user/utils/EmailSender.kt
@@ -1,14 +1,27 @@
-package com.flab.ticketing.user.service
+package com.flab.ticketing.user.utils
 
-import org.springframework.stereotype.Service
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Component
+
+@Component
+class EmailSender(
+    @Value("\${spring.mail.sender}") private val senderEmail : String,
+    private val mailSender : JavaMailSender
+) {
 
 
-@Service
-class EmailService {
+    fun sendEmail(email : String, subject : String, content : String){
+
+        val message = SimpleMailMessage()
+        message.from = senderEmail
+        message.setTo(email)
+        message.text = content
 
 
-    fun getEmailCode(email : String) : String? {
-        return "asdas"
+
+        mailSender.send(message)
     }
 
 }
