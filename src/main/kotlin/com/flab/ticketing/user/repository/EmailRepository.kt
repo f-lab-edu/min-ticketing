bb2a0d675ad15ee1483626bdb92e52ad92c3be53
minturtle <minseok0425@kakao.com>
2024-07-30 11:25:44 +0900

    feat: EmailRepository Bean 추가

--- src/main/kotlin/com/flab/ticketing/user/repository/EmailRepository.kt
+++ src/main/kotlin/com/flab/ticketing/user/repository/EmailRepository.kt
@@ -1,9 +1,11 @@
 package com.flab.ticketing.user.repository
 
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Component
 import java.time.Duration
 import java.time.temporal.ChronoUnit
 
+@Component
 class EmailRepository(
     private val redisTemplate: RedisTemplate<String, String>
 ) {
