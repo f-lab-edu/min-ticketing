cc23854c16f45e1305bed9bc5d41d19d81b3ee09
minturtle <minseok0425@kakao.com>
2024-07-30 13:53:01 +0900

    feat: Embedded Redis 및 greenMailServer 의존성 추가 및 통합테스트 환경 구축

---
+++ src/test/kotlin/com/flab/ticketing/common/BehaviorIntegrationTest.kt
@@ -0,0 +1,75 @@
+package com.flab.ticketing.common
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.icegreen.greenmail.configuration.GreenMailConfiguration
+import com.icegreen.greenmail.util.GreenMail
+import com.icegreen.greenmail.util.ServerSetupTest
+import io.kotest.core.extensions.Extension
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.extensions.spring.SpringExtension
+import jakarta.annotation.PostConstruct
+import jakarta.annotation.PreDestroy
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import redis.embedded.RedisServer
+
+
+@SpringBootTest
+@Import(EmbeddedRedisServerConfig::class)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+abstract class BehaviorIntegrationTest : BehaviorSpec() {
+
+    override fun extensions(): List<Extension> = listOf(SpringExtension)
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
+
+    val greenMail = GreenMail(ServerSetupTest.SMTP_IMAP)
+        .withConfiguration(
+            GreenMailConfiguration
+                .aConfig()
+                .withUser("foo@localhost", "foo", "foo-pwd")
+        )
+
+    @PostConstruct
+    fun setUp() {
+        greenMail.start()
+    }
+
+    @PreDestroy
+    fun tearDown() {
+        greenMail.stop()
+    }
+
+}
+
+
+@TestConfiguration
+internal class EmbeddedRedisServerConfig(
+    @Value("\${spring.data.redis.port}") val port: Int
+) {
+
+    private val redisServer = RedisServer(port)
+
+    @PostConstruct
+    fun runRedisServer() {
+        redisServer.start()
+    }
+
+    @PreDestroy
+    fun stopRedisServer() {
+        redisServer.stop()
+    }
+
+}
