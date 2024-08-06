c2cb5340d1a38bf986a4de6e653a704289e5b58d
minturtle <minseok0425@kakao.com>
2024-08-06 21:15:30 +0900

    feat: UserDetailService 사용자 정보 조회 기능 추가

--- src/main/kotlin/com/flab/ticketing/user/exception/UserErrorInfos.kt
+++ src/main/kotlin/com/flab/ticketing/user/exception/UserErrorInfos.kt
@@ -8,5 +8,6 @@ enum class UserErrorInfos(override val code: String, override val message: Strin
     EMAIL_VERIFYCODE_INVALID("USER-004", "이메일 인증 코드가 잘못되었습니다."),
     EMAIL_NOT_VERIFIED("USER-005", "이메일 인증처리가 완료되지 않았습니다."),
     PASSWORD_CONFIRM_NOT_EQUALS("USER-006", "비밀번호와 비밀번호 확인 값이 동일하지 않습니다."),
-    LOGIN_INFO_INVALID("USER-007", "로그인 정보의 형태가 올바르지 않습니다.")
+    LOGIN_INFO_INVALID("USER-007", "로그인 정보의 형태가 올바르지 않습니다."),
+    LOGIN_FAILED("USER-008", "이메일 혹은 비밀번호가 잘못되었습니다.")
 }
