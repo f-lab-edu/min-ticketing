e1e0305aa1d392622d0ab7aa2e74a920fbd13e28
minturtle <minseok0425@kakao.com>
2024-08-13 15:38:52 +0900

    feat: 공연 정보가 사용자의 limit 이하일때 다음 Cursor를 반환하지 않는 로직 추가

--- src/test/kotlin/com/flab/ticketing/performance/integration/PerformanceSearchIntegrationTest.kt
+++ src/test/kotlin/com/flab/ticketing/performance/integration/PerformanceSearchIntegrationTest.kt
@@ -76,7 +76,58 @@ class PerformanceSearchIntegrationTest : IntegrationTest() {
                 }
             }
         }
-        
+
+        given("공연 정보가 5개 이하로 존재할 시") {
+            val performanceTestDataGenerator = PerformanceTestDataGenerator()
+
+            val performances = performanceTestDataGenerator.createPerformanceGroupbyRegion(
+                performanceCount = 5
+            )
+
+            regionRepository.save(performances[0].performancePlace.region)
+            placeRepository.save(performances[0].performancePlace)
+
+            performances.forEach {
+                performanceRepository.save(it)
+            }
+
+            `when`("사용자가 5개의 공연 정보를 조회할 시") {
+                val uri = "/api/performances?limit=5"
+
+                val mvcResult = mockMvc.perform(
+                    MockMvcRequestBuilders.get(uri)
+                )
+                    .andDo(MockMvcResultHandlers.print())
+                    .andReturn()
+
+
+                then("cursor 정보는 NULL이고, 공연 정보가 존재하는 만큼 반환한다.") {
+                    val actual = objectMapper.readValue<CursoredResponse<PerformanceSearchResult>>(
+                        mvcResult.response.contentAsString,
+                        objectMapper.typeFactory.constructParametricType(
+                            CursoredResponse::class.java,
+                            PerformanceSearchResult::class.java
+                        )
+                    )
+
+                    val expected = performances.map {
+                        PerformanceSearchResult(
+                            it.uid,
+                            it.image,
+                            it.name,
+                            it.performancePlace.region.name,
+                            it.performanceDateTime.minOf { d -> d.showTime },
+                            it.performanceDateTime.maxOf { d -> d.showTime }
+                        )
+                    }.asReversed()
+
+                    actual.cursor shouldBe null
+                    actual.data.size shouldBe 5
+                    actual.data shouldContainExactly expected
+
+                }
+            }
+        }
 
     }
 
