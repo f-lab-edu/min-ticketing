c55c114b9c8825ed28d797175cdb821e3b82cbd2
minturtle <minseok0425@kakao.com>
2024-08-27 22:52:20 +0900

    feat: 결제 승인 시 현재 로그인된 유저와 다른 유저가 승인 시도시 400 BadRequest를 반환하는 기능 추가

--- src/main/kotlin/com/flab/ticketing/order/exception/OrderErrorInfos.kt
+++ src/main/kotlin/com/flab/ticketing/order/exception/OrderErrorInfos.kt
@@ -6,6 +6,7 @@ enum class OrderErrorInfos(override val code: String, override val message: Stri
 
     ALREADY_RESERVED("ORDER-001", "이미 예약되었습니다."),
     INVALID_CART_INFO("ORDER-002", "올바르지 않은 장바구니 정보가 존재합니다."),
-    ORDER_INFO_NOT_FOUND("ORDER-003", "주문 정보를 조회할 수 없습니다.")
+    ORDER_INFO_NOT_FOUND("ORDER-003", "주문 정보를 조회할 수 없습니다."),
+    INVALID_USER("ORDER-004", "현재 로그인된 유저와 주문 정보가 일치하지 않습니다.")
 
 }
