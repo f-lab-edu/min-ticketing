40aa18400284f52d9c6ad3320be89723e00c8e5e
minturtle <minseok0425@kakao.com>
2024-09-28 15:37:16 +0900

    chore: 배포 설정 변경

--- src/main/kotlin/com/flab/ticketing/common/config/RedisConfig.kt
+++ src/main/kotlin/com/flab/ticketing/common/config/RedisConfig.kt
@@ -3,7 +3,9 @@ package com.flab.ticketing.common.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 
 
@@ -14,7 +16,19 @@ class RedisConfig(
 ) {
 
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory {
+    @Profile("prod")
+    fun redisConnectionFactory(@Value("\${spring.data.redis.password}") password: String): RedisConnectionFactory {
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration()
+        redisStandaloneConfiguration.hostName = host
+        redisStandaloneConfiguration.port = port
+        redisStandaloneConfiguration.setPassword(password)
+        return LettuceConnectionFactory(redisStandaloneConfiguration)
+    }
+
+    @Bean
+    @Profile("!prod")
+    fun devRedisConnectionFactory(): RedisConnectionFactory {
         return LettuceConnectionFactory(host, port)
     }
+
 }
