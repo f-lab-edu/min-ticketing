33037d0f13e09583861f78a2564fd89e1e7596c8
minturtle <minseok0425@kakao.com>
2024-10-22 18:32:52 +0900

    feat: 좌석 예매시 Lock을 얻는 기능 추가

---
+++ src/main/kotlin/com/flab/ticketing/order/repository/writer/CartLockingProxy.kt
@@ -0,0 +1,74 @@
+package com.flab.ticketing.order.repository.writer
+
+import com.flab.ticketing.common.exception.CommonErrorInfos
+import com.flab.ticketing.common.exception.DuplicatedException
+import com.flab.ticketing.common.exception.InternalServerException
+import com.flab.ticketing.order.entity.Cart
+import com.flab.ticketing.order.exception.OrderErrorInfos
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.script.RedisScript
+import org.springframework.stereotype.Component
+
+
+@Aspect
+@Component
+class CartLockingProxy(
+    private val redisTemplate: RedisTemplate<String, String>
+
+) {
+    private val LOCK_PREFIX = "cart_lock:"
+    private val LOCK_TIMEOUT_MILLIS = 30L * 60_000L // 30분
+    private val acquireLockScript: RedisScript<Boolean> = setUpLockScript()
+
+
+    @Around("execution(* com.flab.ticketing.order.repository.writer.CartWriter.save(..))")
+    fun acquireLockBeforeSave(joinPoint: ProceedingJoinPoint): Any? {
+        val (key, value) = joinPoint.extractKVFromCart()
+
+        acquireLockOrThrows(key, value)
+
+        return joinPoint.proceed()
+    }
+
+
+    private fun setUpLockScript(): RedisScript<Boolean> {
+        val ACQUIRE_LOCK_SCRIPT = """
+            if redis.call('setnx', KEYS[1], ARGV[1]) == 1 then
+                redis.call('pexpire', KEYS[1], ARGV[2])
+                return 1
+            else
+                return 0
+            end
+        """
+
+        return RedisScript.of(ACQUIRE_LOCK_SCRIPT, Boolean::class.java)
+    }
+
+    private fun ProceedingJoinPoint.extractKVFromCart(): Pair<String, String> {
+        val cart: Cart = this.args
+            .firstOrNull { it is Cart }
+            ?.let { it as Cart } ?: throw InternalServerException(CommonErrorInfos.SERVICE_ERROR)
+
+        val key = "${LOCK_PREFIX}${cart.performanceDateTime.uid}_${cart.seat.uid}"
+        val value = cart.user.uid
+
+        return Pair(key, value)
+    }
+
+    private fun acquireLockOrThrows(key: String, value: String) {
+        val result = redisTemplate.execute(
+            acquireLockScript,
+            listOf(key),    // KEYS[1]
+            value,          // ARGV[1]
+            LOCK_TIMEOUT_MILLIS.toString()   // ARGV[2]
+        )
+
+        if (!result) {
+            throw DuplicatedException(OrderErrorInfos.ALREADY_RESERVED)
+        }
+    }
+
+}
---
+++ src/test/kotlin/com/flab/ticketing/order/service/ReservationServiceLockTest.kt
@@ -0,0 +1,106 @@
+package com.flab.ticketing.order.service
+
+import com.flab.ticketing.common.IntegrationTest
+import com.flab.ticketing.common.PerformanceTestDataGenerator
+import com.flab.ticketing.common.UserTestDataGenerator
+import com.flab.ticketing.common.exception.DuplicatedException
+import com.flab.ticketing.order.entity.Cart
+import com.flab.ticketing.order.repository.CartRepository
+import com.flab.ticketing.performance.entity.Performance
+import com.flab.ticketing.performance.repository.PerformancePlaceRepository
+import com.flab.ticketing.performance.repository.PerformanceRepository
+import com.flab.ticketing.performance.repository.RegionRepository
+import com.flab.ticketing.user.repository.UserRepository
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
+import io.mockk.every
+import io.mockk.verify
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.springframework.beans.factory.annotation.Autowired
+
+class ReservationServiceLockTest : IntegrationTest() {
+
+    @Autowired
+    private lateinit var reservationService: ReservationService
+
+    @MockkBean
+    private lateinit var cartRepository: CartRepository
+
+    @Autowired
+    private lateinit var regionRepository: RegionRepository
+
+    @Autowired
+    private lateinit var placeRepository: PerformancePlaceRepository
+
+    @Autowired
+    private lateinit var performanceRepository: PerformanceRepository
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    init {
+
+        given("특정 공연 정보가 존재할 때") {
+            val performance = PerformanceTestDataGenerator.createPerformance()
+            val user = UserTestDataGenerator.createUser()
+            val performanceDateTime = performance.performanceDateTime[0]
+            val seat = performance.performancePlace.seats[0]
+
+            every { cartRepository.save(any()) } returns Cart("uid", seat, performanceDateTime, user)
+            userRepository.save(user)
+            savePerformance(listOf(performance))
+
+
+            // warm-up
+            reservationService.reserve(user.uid, performance.uid, performanceDateTime.uid, seat.uid)
+
+
+            `when`("사용자가 이미 예약된 좌석에 대해 Redis에 Lock 정보가 남아있다면") {
+                val user2 = UserTestDataGenerator.createUser(uid = "uid", email = "e@e.com")
+
+                userRepository.save(user2)
+                then("DB에 SAVE 요청을 보내지 않고 DuplicatedException을 throw한다.") {
+                    shouldThrow<DuplicatedException> {
+                        reservationService.reserve(
+                            user2.uid,
+                            performance.uid,
+                            performanceDateTime.uid,
+                            seat.uid
+                        )
+                    }
+
+                    verify(exactly = 1) { cartRepository.save(any()) }
+                }
+            }
+
+        }
+    }
+
+
+    override suspend fun afterEach(testCase: TestCase, result: TestResult) {
+        super.afterEach(testCase, result)
+
+        PerformanceTestDataGenerator.reset()
+        withContext(Dispatchers.IO) {
+            userRepository.deleteAll()
+            performanceRepository.deleteAll()
+            placeRepository.deleteAll()
+            regionRepository.deleteAll()
+        }
+
+
+    }
+
+    private fun savePerformance(performances: List<Performance>) {
+        regionRepository.save(performances[0].performancePlace.region)
+        placeRepository.save(performances[0].performancePlace)
+
+        performances.forEach {
+            performanceRepository.save(it)
+        }
+    }
+
+}
