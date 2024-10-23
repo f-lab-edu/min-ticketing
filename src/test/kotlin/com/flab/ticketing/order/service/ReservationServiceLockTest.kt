40b03df9785aff7d2e68b8a4609bc7e63684ca82
minturtle <minseok0425@kakao.com>
2024-10-23 18:03:05 +0900

    feat: 예약할 때 Caching이 되는 지 확인하는 테스트 코드 추가

--- src/test/kotlin/com/flab/ticketing/order/service/ReservationServiceLockTest.kt
+++ src/test/kotlin/com/flab/ticketing/order/service/ReservationServiceLockTest.kt
@@ -12,14 +12,21 @@ import com.flab.ticketing.performance.repository.PerformanceRepository
 import com.flab.ticketing.performance.repository.RegionRepository
 import com.flab.ticketing.user.repository.UserRepository
 import com.ninjasquad.springmockk.MockkBean
+import com.ninjasquad.springmockk.SpykBean
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
 import io.mockk.every
+import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.Cache
+import org.springframework.cache.CacheManager
+import org.springframework.cache.get
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.ValueOperations
 
 class ReservationServiceLockTest : IntegrationTest() {
 
@@ -28,6 +35,9 @@ class ReservationServiceLockTest : IntegrationTest() {
 
     @MockkBean
     private lateinit var cartRepository: CartRepository
+
+    @SpykBean
+    private lateinit var redisTemplate: RedisTemplate<String, String>
 
     @Autowired
     private lateinit var regionRepository: RegionRepository
@@ -77,13 +87,37 @@ class ReservationServiceLockTest : IntegrationTest() {
             }
 
         }
+
+        given("특정 공연 정보가 존재할 때 - Caching 추가 확인"){
+            val performance = PerformanceTestDataGenerator.createPerformance()
+            val user = UserTestDataGenerator.createUser()
+            val performanceDateTime = performance.performanceDateTime[0]
+            val seat = performance.performancePlace.seats[0]
+            val mockRedisOperation = mockk<ValueOperations<String, String>>()
+
+
+            every { redisTemplate.opsForValue() } returns mockRedisOperation
+            every { mockRedisOperation.setIfAbsent(any(), any(), any()) } returns true
+            every { cartRepository.save(any()) } returns Cart("uid", seat, performanceDateTime, user)
+            userRepository.save(user)
+            savePerformance(listOf(performance))
+
+            `when`("사용자가 이미 예약된 좌석에 대해 Redis에 Lock 정보가 남아있다면") {
+                reservationService.reserve(user.uid, performance.uid, performanceDateTime.uid, seat.uid)
+
+                then("Cache를 추가해 저장한다."){
+                    val key = "lock:${seat.uid}_${performanceDateTime.uid}"
+                    val value = user.uid
+                    verify(exactly = 1) { mockRedisOperation.setIfAbsent(key, value, any()) }
+                }
+            }
+
+        }
     }
 
 
     override suspend fun afterEach(testCase: TestCase, result: TestResult) {
         super.afterEach(testCase, result)
-
-        PerformanceTestDataGenerator.reset()
         withContext(Dispatchers.IO) {
             userRepository.deleteAll()
             performanceRepository.deleteAll()
