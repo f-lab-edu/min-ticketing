0ac441791879184ce5323e1725d45316c6f88e2f
minturtle <minseok0425@kakao.com>
2024-11-25 21:43:46 +0900

    feat: OCIFileService 개발 완료

--- src/main/kotlin/com/flab/ticketing/common/config/OCIClientConfig.kt
+++ src/main/kotlin/com/flab/ticketing/common/config/OCIClientConfig.kt
@@ -1,5 +1,7 @@
 package com.flab.ticketing.common.config
 
+import com.flab.ticketing.common.service.FileService
+import com.flab.ticketing.common.service.OciFileService
 import com.oracle.bmc.ConfigFileReader
 import com.oracle.bmc.Region
 import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider
@@ -9,6 +11,7 @@ import com.oracle.bmc.objectstorage.transfer.UploadManager
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
 
 
@@ -16,6 +19,9 @@ import org.springframework.context.annotation.Profile
 @Profile("!test")
 class OCIClientConfig(
     @Value("\${oci.config-file.path}") private val configFilePath: String,
+    @Value("\${oci.region-name}") private val regionName: String,
+    @Value("\${oci.namespace-name}") private val nameSpaceName: String,
+    @Value("\${oci.bucket-name}") private val bucketName: String,
     @Value("\${oci.config-file.profile:DEFAULT}") private val configFileProfile: String
 ) {
 
@@ -28,7 +34,7 @@ class OCIClientConfig(
 
         // REGION 변경 가능성 고려 필요
         val client = ObjectStorageClient.builder()
-            .region(Region.AP_CHUNCHEON_1)
+            .region(Region.valueOf(regionName))
             .build(provider)
 
         val uploadConfiguration = UploadConfiguration.builder()
@@ -37,6 +43,19 @@ class OCIClientConfig(
             .build()
 
         return UploadManager(client, uploadConfiguration)
+    }
+
+    @Bean
+    @Primary
+    fun ociFileService(
+        uploadManager: UploadManager
+    ): FileService {
+        return OciFileService(
+            uploadManager = uploadManager,
+            regionName = regionName,
+            nameSpaceName = nameSpaceName,
+            bucketName = bucketName
+        )
     }
 
 }
